0dc1ed3341a19437e3c9e93f6304e2dbe2597fff
Ricardo Cosme <ricardo@cosme.im>
2021-03-29 16:26:20 -0300

    Add ISR macros to ATmega328P

--- include/avr/interrupt/mcu.hpp
+++ include/avr/interrupt/mcu.hpp
@@ -4,4 +4,6 @@
 #include "avr/interrupt/mcu/attiny13a.h"
 #elif __AVR_ATtiny25__ || __AVR_ATtiny45__ || __AVR_ATtiny85__
 #include "avr/interrupt/mcu/attiny85.h"
+#elif __AVR_ATmega328P__ || __AVR_ATmega328__
+#include "avr/interrupt/mcu/atmega328p.h"
 #endif
