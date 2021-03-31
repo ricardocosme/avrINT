1dae3d1662a252c3f68416f6d6687f951152c7c2
Ricardo Cosme <ricardo@cosme.im>
2021-03-31 13:00:44 -0300

    RAII interruptible
    Sometimes a function takes the responsability of turn on the
    interruptions but is to strong to assume that it should turn off at
    the end or worse than that it should leave the interruptions turned
    on.

--- include/avr/interrupt.hpp
+++ include/avr/interrupt.hpp
@@ -64,9 +64,9 @@ struct on_at_the_end_t{};
     
     By default the state of the status register that was before the
     atomic scope is recovered at the end of the scope, which means
-    that interrupts are enabled only if they were enabled before. If
-    the argument 'on_at_the_end' is passed then the interrupts are
-    always enabled at the end of the scope.
+    that interruptions are enabled only if they were enabled
+    before. If the argument 'on_at_the_end' is passed then the
+    interruptions are always enabled at the end of the scope.
 
     Example:
       {
@@ -110,13 +110,71 @@ public:
     }
 };
 
+/** Tag to be used as a parameter to the constructor of the class
+ * 'interruptible'. It informs that the global interrupts must always
+ * be disabled at the end of the scope. */
+struct off_at_the_end_t{};
+
+/** RAII to turn on interruptions in the beginning and restore SREG
+    state or turn off at the end.
+    
+    By default the state of the status register that was before the
+    interruptible scope is recovered at the end of the scope, which
+    means that interruptions are disabled only if they were disabled
+    before. If the argument 'off_at_the_end' is passed then the
+    interruptions are always disabled at the end of the scope.
+
+    Example:
+      {
+        interruptible si; //same as 'interruptible sa(restore)'
+        //my code
+      }
+   
+      {
+        interruptible si(off_at_the_end);
+        //my code
+      }
+
+    Note, the compiler optimizer can reorder instructions around the
+    interruptible scope when that instructions doesn't have observable
+    side effects. Take a look at 'reordering_code.org' to know more
+    about it.
+
+    [optional] clobbers: list of lvalues that can have an observable
+    side effect. This should only be used to do fine adjustments in
+    the generated code in order to move to the outside of the atomic
+    section some code that does't need to be there and, first of
+    all, when the timing is a critical factor.
+*/
+template<typename AtTheEnd = restore_t>
+class interruptible {
+    uint8_t _sreg;
+public:
+    constexpr static bool is_restore
+        {detail::is_same<AtTheEnd, restore_t>::value};
+
+    template<typename... Clobbers>
+    explicit interruptible(AtTheEnd = AtTheEnd{}, Clobbers&... clobbers) noexcept {
+        clobber(clobbers...);
+        if(is_restore) _sreg = io::sreg;
+        on();
+    }
+    
+    ~interruptible() noexcept {
+        if(is_restore) io::sreg = _sreg;
+        else off();
+    }
+};
+
 #if __cplusplus >= 201703L
 inline constexpr restore_t restore;
 inline constexpr on_at_the_end_t on_at_the_end;
+inline constexpr off_at_the_end_t off_at_the_end;
 #else
 namespace {
 constexpr auto& restore{detail::global<restore_t>::instance};
 constexpr auto& on_at_the_end{detail::global<on_at_the_end_t>::instance};
+constexpr auto& off_at_the_end{detail::global<off_at_the_end_t>::instance};
 }//anonymous namespace
 #endif
 
@@ -125,6 +183,12 @@ template<typename AtTheEnd = restore_t>
 [[gnu::always_inline, nodiscard]]
 inline atomic<AtTheEnd> make_atomic(AtTheEnd = AtTheEnd{}) noexcept
 { return atomic<AtTheEnd>{}; }
+
+/** [C++11/14] The interruptinle ctor should be use if C++17 is available. */
+template<typename AtTheEnd = restore_t>
+[[gnu::always_inline, nodiscard]]
+inline interruptible<AtTheEnd> make_interruptible(AtTheEnd = AtTheEnd{}) noexcept
+{ return interruptible<AtTheEnd>{}; }
 
 /** Defines an ISR(Interrupt Service Routine)
     
