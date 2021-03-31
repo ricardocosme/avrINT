3597cc3676199748ceebf9c8bd68ffc95983aa89
Ricardo Cosme <ricardo@cosme.im>
2021-03-31 19:17:31 -0300

    Rename .h to .hpp

--- include/avr/interrupt/mcu.hpp
+++ include/avr/interrupt/mcu.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
 #if __AVR_ATtiny13A__
-#include "avr/interrupt/mcu/attiny13a.h"
+#include "avr/interrupt/mcu/attiny13a.hpp"
 #elif __AVR_ATtiny25__ || __AVR_ATtiny45__ || __AVR_ATtiny85__
-#include "avr/interrupt/mcu/attiny85.h"
+#include "avr/interrupt/mcu/attiny85.hpp"
 #elif __AVR_ATmega328P__ || __AVR_ATmega328__
-#include "avr/interrupt/mcu/atmega328p.h"
+#include "avr/interrupt/mcu/atmega328p.hpp"
 #endif
